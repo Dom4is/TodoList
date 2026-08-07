cfb3f98a5f57435ca133a8b00537fb264f412867
Dom4is <damir.tulemisov@yandex.ru>
2026-08-07 16:10:58 +0500

    add useLocalStorage hook

---
+++ src/shared/hooks/useLocalStorage/useLocalStorage.ts
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+
+export default function useLocalStorage<T>(
+  key: string,
+  initialValue: T,
+): [T, (value: T) => void] {
+  const [storedValue, setStoredValue] = useState<T>(() => {
+    try {
+      //Читаем значение localStorage или берем начальное
+      const localStorageItem = window.localStorage.getItem(key);
+
+      return localStorageItem
+        ? (JSON.parse(localStorageItem) as T)
+        : initialValue;
+    } catch (error) {
+      console.log(`Error reading localStorage key: ${key}, ${error}`);
+      return initialValue;
+    }
+  });
+
+  useEffect(() => {
+    try {
+      //При изменении сетим значение в localStorage
+      const localStorageItem = JSON.stringify(storedValue);
+
+      window.localStorage.setItem(key, localStorageItem);
+    } catch (error) {
+      console.log(`Error setting localStorage key: ${key}, ${error}`);
+    }
+  }, [key, storedValue]);
+
+  const setValue = (value: T) => {
+    setStoredValue(value);
+  };
+
+  return [storedValue, setValue];
+}
